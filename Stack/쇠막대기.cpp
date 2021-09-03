9548e1ffd8f2f907d15530280f17d91294f38d18
heemang2001 <babypooh2001@naver.com>
2021-09-03 16:19:22 +0900

    쇠막대기

--- Stack/쇠막대기.cpp
+++ Stack/쇠막대기.cpp
@@ -18,6 +18,7 @@ int main()
 
 	for (int i = 0; i < sTemp.size(); i++)
 	{	
+		// 여는 괄호면 스택에 추가
 		if (sTemp[i] == '(')
 		{
 			stk.push(sTemp[i]);
@@ -25,13 +26,16 @@ int main()
 
 		else
 		{
+			// 닫는 괄호면 스택에서 하나제거
 			stk.pop();
 
+			// 전 기호가 여는기호였으면 레이저였으므로 열린괄호만큼 개수추가
 			if (sTemp[i - 1] == '(')
 			{
 				answer += stk.size();
 			}
 
+			// 전 기호가 닫는 기호면 쇠막대기의 마지막이므로 1개추가
 			else
 			{
 				answer++;
